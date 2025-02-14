7bc0ba6585c5633299b19e7efe3333c02f60e04f
Ankit Bhattarai <ankitbhattarai76@gmail.com>
2025-02-14 00:41:10 -0500

    movie models
    movie models & database set up on admin page

--- gt_movie_store/settings.py
+++ gt_movie_store/settings.py
@@ -127,3 +127,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / 'gt_movie_store/static/',
 ]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
