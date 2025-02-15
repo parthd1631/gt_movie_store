294366766ffe8c2cd291450e8758b15681ce90ba
Krish Bhalodiya <k2005bhalodiya@gmail.com>
2025-02-15 05:21:10 -0500

    Fixed movie image background issue on movie page

--- cart/templatetags/cart_filters.py
+++ cart/templatetags/cart_filters.py
@@ -8,3 +8,7 @@ def get_cart_quantity(cart, movie_id):
 @register.filter(name='get_total_price')
 def get_total_price(cart, movie_id):
     return cart[str(movie_id)] * get_cart_quantity(cart, movie_id)
+
+@register.filter(name='multiply')
+def multiply(value, arg):
+    return value * arg
