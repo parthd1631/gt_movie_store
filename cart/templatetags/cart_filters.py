d7138215d9e6299f71b30b2c4aaf72ef40986f10
Krish Bhalodiya <k2005bhalodiya@gmail.com>
2025-02-15 02:59:44 -0500

    Better frontend for Home and Movies

--- cart/templatetags/cart_filters.py
+++ cart/templatetags/cart_filters.py
@@ -4,3 +4,7 @@ register = template.Library()
 @register.filter(name='get_quantity')
 def get_cart_quantity(cart, movie_id):
     return cart[str(movie_id)]
+
+@register.filter(name='get_total_price')
+def get_total_price(cart, movie_id):
+    return cart[str(movie_id)] * get_cart_quantity(cart, movie_id)
